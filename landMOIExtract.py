becc20aa45893cff345f39148c3a96d9173609f0
karang <gabriel.g.gras@gmail.com>
2020-05-22 08:00:06 +0800

    fully functional and headless. No error or exception handling.

--- landMOIExtract.py
+++ landMOIExtract.py
@@ -9,14 +9,19 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 
 def landMoi(city, town, road, lane, alley, number):
+    sleep_time = 2
     # dictionary to store extracted information
     vars = {}
 
     # start the driver (this could be moved outside if called many times)
-    driver = webdriver.Chrome('./chromedriver')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 
     # entry point
     driver.get("http://easymap.land.moi.gov.tw/")
@@ -37,9 +42,12 @@ def landMoi(city, town, road, lane, alley, number):
     driver.find_element(By.ID, "doorPlateTypeId").click()
     dropdown = driver.find_element(By.ID, "select_city_id1")
 
-    # select city (must be exactly as found in the dropdown)
-    dropdown.find_element(
-        By.XPATH, "//option[. = '{1}']".format(str(city))).click()
+    # select city (must be exactly as found in the dropdown
+    city_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    for option in city_options:
+        if(option.text == str(city)):
+            option.click()
     element = driver.find_element(By.ID, "select_city_id1")
     actions = ActionChains(driver)
     actions.move_to_element(element).click_and_hold().perform()
@@ -51,13 +59,17 @@ def landMoi(city, town, road, lane, alley, number):
     actions.move_to_element(element).release().perform()
 
     # wait for page to load next options (time could be calibrated)
-    time.sleep(5000)
+    time.sleep(sleep_time)
     driver.find_element(By.ID, "select_city_id1").click()
     dropdown = driver.find_element(By.ID, "select_town_id1")
 
     # select town
-    dropdown.find_element(
-        By.XPATH, "//option[. = '{1}']".format(str(town))).click()
+    town_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    for option in town_options:
+        if(option.text == str(town)):
+            option.click()
+
     element = driver.find_element(By.ID, "select_town_id1")
     actions = ActionChains(driver)
     actions.move_to_element(element).click_and_hold().perform()
@@ -69,13 +81,17 @@ def landMoi(city, town, road, lane, alley, number):
     actions.move_to_element(element).release().perform()
 
     # wait for page to load
-    time.sleep(5000)
+    time.sleep(sleep_time)
     driver.find_element(By.ID, "select_town_id1").click()
     dropdown = driver.find_element(By.ID, "select_road_id")
 
     # select road
-    dropdown.find_element(
-        By.XPATH, "//option[. = '{1}']".format(str(road))).click()
+    road_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    for option in road_options:
+        if(option.text == str(road)):
+            option.click()
+
     element = driver.find_element(By.ID, "select_road_id")
     actions = ActionChains(driver)
     actions.move_to_element(element).click_and_hold().perform()
@@ -87,13 +103,17 @@ def landMoi(city, town, road, lane, alley, number):
     actions.move_to_element(element).release().perform()
 
     # wait for page to load
-    time.sleep(5000)
+    time.sleep(sleep_time)
     driver.find_element(By.ID, "select_road_id").click()
     dropdown = driver.find_element(By.ID, "select_lane_id")
 
     # select lane
-    dropdown.find_element(
-        By.XPATH, "//option[. = '{1}']".format(str(lane))).click()
+    lane_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    for option in lane_options:
+        if(option.text == str(lane)):
+            option.click()
+
     element = driver.find_element(By.ID, "select_lane_id")
     actions = ActionChains(driver)
     actions.move_to_element(element).click_and_hold().perform()
@@ -105,13 +125,17 @@ def landMoi(city, town, road, lane, alley, number):
     actions.move_to_element(element).release().perform()
 
     # wait for page to load
-    time.sleep(5000)
+    time.sleep(sleep_time)
     driver.find_element(By.ID, "select_lane_id").click()
     dropdown = driver.find_element(By.ID, "select_alley_id")
 
     # select alley
-    dropdown.find_element(
-        By.XPATH, "//option[. = '{1}']".format(str(alley))).click()
+    alley_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    for option in alley_options:
+        if(option.text == str(alley)):
+            option.click()
+
     element = driver.find_element(By.ID, "select_alley_id")
     actions = ActionChains(driver)
     actions.move_to_element(element).click_and_hold().perform()
@@ -132,9 +156,37 @@ def landMoi(city, town, road, lane, alley, number):
     actions.move_to_element(element).perform()
 
     # wait for page to load (this can take a while)
-    time.sleep(15000)
+    # time.sleep(30)
+    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located(
+        (By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(1) > td")))
 
     # extract some values a print them
-    vars["extractedValue"] = driver.find_element(
+    vars["行政區"] = driver.find_element(
         By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(1) > td").text
-    print(vars["extractedValue"])
+    vars["地政事務所"] = driver.find_element(
+        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(2) > td").text
+    vars["地段	"] = driver.find_element(
+        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(3) > td").text
+    vars["地號"] = driver.find_element(
+        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(4) > td").text
+    vars["面積"] = driver.find_element(
+        By.CSS_SELECTOR, "tr:nth-child(5) > td:nth-child(2)").text
+    vars["公告土地現值"] = driver.find_element(
+        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(6) > td").text
+    vars["公告土地地價"] = driver.find_element(
+        By.CSS_SELECTOR, "tr:nth-child(7) > td:nth-child(2)").text
+    print(vars)
+
+
+def testLandMoi():
+    test_city = '新北市'
+    test_town = '永和區'
+    test_road = '環河西路一段'
+    test_lane = '95巷'
+    test_alley = '19弄'
+    test_number = '1'
+    landMoi(test_city, test_town, test_road,
+            test_lane, test_alley, test_number)
+
+
+testLandMoi()
