eb06bd8bda2f148ebc725489028d6cbb3ae95e40
karang <gabriel.g.gras@gmail.com>
2020-06-15 11:00:23 +0800

    basic functionality is working. Needs some tweaking by a local person.

--- AddressTreeExtract.py
+++ AddressTreeExtract.py
@@ -1,7 +1,7 @@
 #
 # This site is very unreliable (must come up with a way to check the loading time and give up sometimes)
 #
-
+import json
 import pytest
 import pandas as pd
 import time
@@ -14,12 +14,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+sleep_time = 5
 
-
-def landMoi(city, town, road, lane, alley, number, driver):
-    sleep_time = 2
+def getCities(driver):
     # dictionary to store extracted information
-    vars = {}
 
     # entry point
     driver.get("http://easymap.land.moi.gov.tw/")
@@ -43,6 +41,20 @@ def landMoi(city, town, road, lane, alley, number, driver):
     # select city (must be exactly as found in the dropdown
     city_options = dropdown.find_elements(
         By.TAG_NAME, "option")
+    cities = []
+    for option in city_options:
+        city = option.text
+        cities.append(city)
+    return cities
+           
+def getTowns(city,driver):
+    # dictionary to store extracted information
+    # entry point
+   
+    # select city (must be exactly as found in the dropdown
+    dropdown = driver.find_element(By.ID, "select_city_id1")
+    city_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
     for option in city_options:
         if(option.text == str(city)):
             option.click()
@@ -59,6 +71,18 @@ def landMoi(city, town, road, lane, alley, number, driver):
     # wait for page to load next options (time could be calibrated)
     time.sleep(sleep_time)
     driver.find_element(By.ID, "select_city_id1").click()
+    dropdown = driver.find_element(By.ID, "select_town_id1")
+
+    # select town
+    town_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    towns = []
+    for option in town_options:
+        towns.append(option.text)
+    return towns
+
+def getRoads(town, driver):
+
     dropdown = driver.find_element(By.ID, "select_town_id1")
 
     # select town
@@ -86,6 +110,18 @@ def landMoi(city, town, road, lane, alley, number, driver):
     # select road
     road_options = dropdown.find_elements(
         By.TAG_NAME, "option")
+    roads=[]
+    for option in road_options:
+        roads.append(option.text)
+    return roads
+
+def getLanes( road, driver):
+     
+    dropdown = driver.find_element(By.ID, "select_road_id")
+
+    # select road
+    road_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
     for option in road_options:
         if(option.text == str(road)):
             option.click()
@@ -103,6 +139,17 @@ def landMoi(city, town, road, lane, alley, number, driver):
     # wait for page to load
     time.sleep(sleep_time)
     driver.find_element(By.ID, "select_road_id").click()
+    dropdown = driver.find_element(By.ID, "select_lane_id")
+
+    # select lane
+    lane_options = dropdown.find_elements(
+        By.TAG_NAME, "option")
+    lanes = []
+    for option in lane_options:
+        lanes.append(option.text)
+    return lanes
+
+def getAlleys(lane, driver):
     dropdown = driver.find_element(By.ID, "select_lane_id")
 
     # select lane
@@ -130,80 +177,45 @@ def landMoi(city, town, road, lane, alley, number, driver):
     # select alley
     alley_options = dropdown.find_elements(
         By.TAG_NAME, "option")
+    alleys = []
     for option in alley_options:
-        if(option.text == str(alley)):
-            option.click()
+        alleys.append(option.text)
+    return alleys
 
-    element = driver.find_element(By.ID, "select_alley_id")
-    actions = ActionChains(driver)
-    actions.move_to_element(element).click_and_hold().perform()
-    element = driver.find_element(By.ID, "select_alley_id")
-    actions = ActionChains(driver)
-    actions.move_to_element(element).perform()
-    element = driver.find_element(By.ID, "select_alley_id")
-    actions = ActionChains(driver)
-    actions.move_to_element(element).release().perform()
-    driver.find_element(By.ID, "select_alley_id").click()
-    driver.find_element(By.ID, "doorNoId").click()
+def save_json(dict):
+    print(dict)
+    json_dict = json.dumps(dict, ensure_ascii=False)
+    f = open("AddressTree.json", "w", encoding="utf-8")
+    f.write(json_dict)
+    f.close()
 
-    # enter door number
-    driver.find_element(By.ID, "doorNoId").send_keys(str(number))
-    driver.find_element(By.ID, "door_botton").click()
-    element = driver.find_element(By.ID, "button_cada")
-    actions = ActionChains(driver)
-    actions.move_to_element(element).perform()
-
-    # wait for page to load (this can take a while)
-    # time.sleep(30)
-    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located(
-        (By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(1) > td")))
-
-    # extract some values a print them
-    vars["行政區"] = driver.find_element(
-        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(1) > td").text
-    vars["地政事務所"] = driver.find_element(
-        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(2) > td").text
-    vars["地段"] = driver.find_element(
-        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(3) > td").text
-    vars["地號"] = driver.find_element(
-        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(4) > td").text
-    vars["面積"] = driver.find_element(
-        By.CSS_SELECTOR, "tr:nth-child(5) > td:nth-child(2)").text
-    vars["公告土地現值"] = driver.find_element(
-        By.CSS_SELECTOR, "#one-column-emphasis tr:nth-child(6) > td").text
-    vars["公告土地地價"] = driver.find_element(
-        By.CSS_SELECTOR, "tr:nth-child(7) > td:nth-child(2)").text
-    return vars["行政區"],  vars["地政事務所"], vars["地段"], vars["地號"], vars["面積"], vars["公告土地現值"], vars["公告土地地價"]
-
-
-def testLandMoi():    
-    driver = webdriver.Chrome('./chromedriver')
-
-    test_city = '新北市'
-    test_town = '永和區'
-    test_road = '環河西路一段'
-    test_lane = '95巷'
-    test_alley = '19弄'
-    test_number = '1'
-    landMoi(test_city, test_town, test_road,
-            test_lane, test_alley, test_number, driver)
-
-#testLandMoi()
-
-# main method to process the csv
-def process_csv(in_csv_path, out_csv_path):
+def startGet():
     # start the driver 
     driver = webdriver.Chrome('./chromedriver')
+    cities = getCities(driver)
+    print(cities)
+    tree = {}
+    try:
+        for city in cities:
+            towns = getTowns(city,driver)
+            print(towns)
+            tree[city] = {}
+            for town in towns:
+               roads = getRoads(town, driver)
+               print(roads)
+               tree[city][town] = roads
+            #    for road in roads:
+            #        lanes = getLanes(road, driver)
+            #        tree[city][town][road] = {}
+            #        for lane in lanes:
+            #            alleys = getAlleys(lane, driver)
+            #            tree[city][town][road][lane] = alleys
 
-     # looks up the actual data we need
-    def lookupInfo(row):
-      return landMoi(row["city"],row["town"],row["road"],row["lane"],row["alley"],row["number"],driver)
+    except:
+        save_json(tree)
+        return tree
+    save_json(tree)
+    return tree
 
-    # read csv and apply the lookupInfo function on each row (rows names must be "ID" and "birth_year")
-    df = pd.read_csv(in_csv_path)
-    df[["行政區","地政事務所","地段","地號","面積","公告土地現值","公告土地地價"]] =df.apply(lookupInfo, axis=1, result_type="expand")  
-     #print(df)
-    df.to_csv(out_csv_path)
-
-# run the main function specifing the input csv and output csv
-process_csv("./sampleData.csv","sampleData_modified.csv")
+tree = startGet()
+print(tree)
---
+++ parseAddresses.py
@@ -0,0 +1,87 @@
+import pandas as pd 
+import re
+
+df = pd.read_csv('testing_ds_20200529.csv')
+addresses = df['address']
+
+test_ad = addresses[0]
+test1 = "510彰化縣 員林鎮 浮圳里3鄰員東路一段171巷24弄10號" # section lane alley number
+test2 = "515彰化縣 大村鄉 過溝村5鄰過溝三巷2號" # village section
+
+def regex_matcher(regex):
+    def matcher(string):
+        string = string.strip()
+        match = re.match(regex, string)
+        matched = match.group(0)
+        return(matched,string.replace(matched, ""))
+    return matcher
+
+def contains_regex(regex):
+    def contains(string):
+        return bool(re.search(regex,string))
+    return contains
+
+contains_postal_code = contains_regex(r'^\d{3,3}')
+get_postal_code = regex_matcher(r'\d{3,3}')
+    
+contains_city = contains_regex(r'\w*[市縣]')
+get_city = regex_matcher(r'\w*[市縣]')
+
+contains_village = contains_regex(r'\w*[村里]')
+get_village = regex_matcher(r'\w*[村里]')
+
+contains_district = contains_regex(r'\w*[區鄉鎮市]')
+get_district = regex_matcher(r'\w*[區鄉鎮市]')
+
+contains_street = contains_regex(r'\w*段|\w*路|[^\d]*(?=\d*)')
+get_street = regex_matcher(r'\w*段|\w*路|[^\d]*(?=\d*)')
+
+contains_alley = contains_regex(r'\w*巷')
+get_alley = regex_matcher(r'\w*巷')
+
+contains_lane = contains_regex(r'\w*弄')
+get_lane = regex_matcher(r'\w*弄')
+
+contains_number = contains_regex(r'\w*號')
+get_number = regex_matcher(r'\w*號')
+
+contains_floor = contains_regex(r'\w*樓')
+get_floor = regex_matcher(r'\w*樓')
+
+def parse_address(adr_string):
+    print("\n")
+    print(adr_string)
+    fields = {}
+    if contains_postal_code(adr_string):
+        (postal_code, adr_string) = get_postal_code(adr_string)
+        fields["postal code"] = postal_code
+    if contains_city(adr_string):
+        (city, adr_string) = get_city(adr_string)
+        fields["city"] = city
+    if contains_district(adr_string):
+        (district, adr_string) = get_district(adr_string)
+        fields["discrict"] = district
+    if contains_village(adr_string):
+        (village, adr_string) = get_village(adr_string)
+        fields["village"] = village
+    if contains_street(adr_string):
+        (street, adr_string) = get_street(adr_string)
+        fields["street"] = street
+    if contains_alley(adr_string):
+        (alley, adr_string) = get_alley(adr_string)
+        fields["alley"] = alley
+    if contains_lane(adr_string):
+        (lane, adr_string) = get_lane(adr_string)
+        fields["lane"] = lane
+    if contains_number(adr_string):
+        (number, adr_string) = get_number(adr_string)
+        fields["number"] = number
+    if contains_floor(adr_string):
+        (floor, adr_string) = get_floor(adr_string)
+        fields["floor"] = floor
+    print(fields)
+
+
+parse_address(test_ad)
+parse_address(test1)
+parse_address(test2)
